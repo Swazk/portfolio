a824c2fde226b82fcb8f5eddddf6f3118cc9606e
Swazk <146499571+Swazk@users.noreply.github.com>
2025-12-27 14:24:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,6 @@
-document.querySelectorAll("a[href^='#']").forEach(anchor => {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute("href"))
-      .scrollIntoView({ behavior: "smooth" });
+function goTo(id) {
+  document.querySelectorAll('.screen').forEach(screen => {
+    screen.classList.remove('active');
   });
-});
-
-const sections = document.querySelectorAll("section");
-const navLinks = document.querySelectorAll(".nav-links a");
-
-window.addEventListener("scroll", () => {
-  let current = "";
-
-  sections.forEach(section => {
-    const sectionTop = section.offsetTop - 90;
-    if (scrollY >= sectionTop) {
-      current = section.getAttribute("id");
-    }
-  });
-
-  navLinks.forEach(link => {
-    link.classList.remove("active");
-    if (link.getAttribute("href") === `#${current}`) {
-      link.classList.add("active");
-    }
-  });
-});
+  document.getElementById(id).classList.add('active');
+}
