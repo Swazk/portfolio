2d8d5f11bb707e2a6c819886e4a0daae58070e01
Swazk <146499571+Swazk@users.noreply.github.com>
2026-03-16 00:12:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,103 +8,52 @@ document.getElementById(id).classList.add("active")
 }
 
 function openCert(file){
+
 window.open(file)
+
 }
 
 GitHubCalendar(".calendar","Swazk")
-
-const scene=new THREE.Scene()
-
-const camera=new THREE.PerspectiveCamera(
-75,
-window.innerWidth/window.innerHeight,
-0.1,
-1000
-)
-
-const renderer=new THREE.WebGLRenderer({
-canvas:document.querySelector('#galaxy'),
-alpha:true
-})
-
-renderer.setSize(window.innerWidth,window.innerHeight)
-
-camera.position.z=5
-
-const starsGeometry=new THREE.BufferGeometry()
-
-const stars=4000
-
-const positions=[]
-
-for(let i=0;i<stars;i++){
-
-positions.push((Math.random()-0.5)*200)
-positions.push((Math.random()-0.5)*200)
-positions.push((Math.random()-0.5)*200)
-
-}
-
-starsGeometry.setAttribute(
-'position',
-new THREE.Float32BufferAttribute(positions,3)
-)
-
-const starsMaterial=new THREE.PointsMaterial({
-color:0xffffff,
-size:0.7
-})
-
-const starMesh=new THREE.Points(starsGeometry,starsMaterial)
-
-scene.add(starMesh)
-
-function animate(){
-
-requestAnimationFrame(animate)
-
-starMesh.rotation.y+=0.0005
-
-renderer.render(scene,camera)
-
-}
-
-animate()
 
 const icon=document.getElementById("chatIcon")
 const chat=document.getElementById("chatWindow")
 
 icon.onclick=()=>{
-chat.style.display=chat.style.display==="block"?"none":"block"
+
+chat.style.display=
+chat.style.display==="block"?"none":"block"
+
 }
 
 const input=document.getElementById("chatInput")
 
-input.addEventListener("keypress",function(e){
+input.addEventListener("keypress",async function(e){
 
 if(e.key==="Enter"){
 
-let text=input.value.toLowerCase()
+let text=input.value
 
 document.getElementById("chatMessages").innerHTML+=
 "<p><b>You:</b>"+text+"</p>"
 
-let reply="I can tell you about Swathi's projects, skills, certificates or contact."
+const res=await fetch("http://localhost:3000/chat",{
 
-if(text.includes("project"))
-reply="Swathi built AI chatbot, fraud detection system and MERN freelancing platform."
+method:"POST",
 
-if(text.includes("skill"))
-reply="Python, Java, JavaScript, React, Node, Cloud and AI."
+headers:{
+"Content-Type":"application/json"
+},
 
-if(text.includes("certificate"))
-reply="She has certifications from AWS, Cisco, Oracle, TCS, Deloitte and JPMorgan."
+body:JSON.stringify({
+message:text
+})
 
-if(text.includes("contact"))
-reply="Email: [swathisenthilkumar017@gmail.com](mailto:swathisenthilkumar017@gmail.com)"
+})
+
+const data=await res.json()
 
 document.getElementById("chatMessages").innerHTML+=
-"<p><b>AI:</b>"+reply+"</p>"
+"<p><b>AI:</b>"+data.reply+"</p>"
 
 input.value=""
 
